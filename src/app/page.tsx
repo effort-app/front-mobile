787f75cc9e83b47f3ed60a85b108accd039ff833
andremartyns <andremartyns@outlook.com>
2023-05-22 21:20:53 -0300

    feature/visual-flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 'use client';
-import Image from 'next/image'
-
-// import { Navbar, Text, Input } from '@nextui-org/react'
-
-import Input from 'antd/lib/input'
-
 
 export default function Home() {
   return (
     
-    <main className="flex min-h-screen flex-col items-center justify-between p-5 bg-black">
+    <main className="flex min-h-screen flex-col items-center justify-between p-5 bg-neutral-900">
       <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
         <div className="flex flex-col items-center justify-center w-full">
-          <Input className='bg-black'/>
+          <div className="flex flex-row items-center justify-center w-full">
+          </div>
+          <div className="flex flex-row items-center justify-center w-full">
+            <h1 className="text-2xl font-bold text-center text-neutral-600">Como está sendo seu dia?</h1>
+          </div>
+          <div className="flex flex-row items-center justify-center w-full fixed bottom-0 w-full p-4">
+            <input 
+              className="w-screen h-12 bg-neutral-700 hover:bg-neutral-800 text-white font-light py-2 px-4 rounded-xl drop-shadow-xl placeholder-neutral-400" 
+              placeholder="Escreva aqui..."
+            />
+          </div>
         </div>
       </div>
     </main>
